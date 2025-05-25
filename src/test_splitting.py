05968dc6f0b74dd99a4cc733e4a817b8911ccec4
TobiasPartzsch <Tobias.Partzsch@gmx.de>
2025-05-25 13:53:37 +0200

    tests for splitting

---
+++ src/test_splitting.py
@@ -0,0 +1,19 @@
+import unittest
+from textnode import DELIMITERS
+
+
+class TestSplitting(unittest.TestCase):
+    def test_split_node_on_delimiter(self):
+        for text_type, delimiter in DELIMITERS.items():
+            with self.subTest(text_type=text_type, delimiter=delimiter):
+                # No delimiter present (should remain unchanged)
+
+                # Single pair of delimiters (should split into three nodes)
+
+                # Multiple pairs (should alternate correctly)
+
+                # Umatched delimiter (should raise an exception)
+                invalid_node = TextNode("I am invalid! {delimiter} because I don't close", text_type)
+                with self.assertRaises(ValueError) as cm:
+                    text_node_to_html_node(invalid_node)
+                self.assertEqual(str(cm.exception), UNMATCHED_DELIMITER_ERROR_MSG.format(delimiter=delimiter, text=invalid_node.text))
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -101,23 +101,6 @@ class TestTextNode(unittest.TestCase):
             text_node_to_html_node(invalid_node)
         self.assertEqual(str(cm.exception), f"Unkown text type: {invalid_node.text_type}")
 
-    # def test_split_node_on_delimiter(self):
-    #     for text_type, delimiter in DELIMITERS.items():
-    #         with self.subTest(text_type=text_type, delimiter=delimiter):
-    #             # No delimiter present (should remain unchanged)
-
-    #             # Single pair of delimiters (should split into three nodes)
-
-    #             # Multiple pairs (should alternate correctly)
-
-    #             # Umatched delimiter (should raise an exception)
-    #             invalid_node = TextNode("I am invalid! {delimiter} because I don't close", text_type)
-    #             with self.assertRaises(ValueError) as cm:
-    #                 text_node_to_html_node(invalid_node)
-    #             self.assertEqual(str(cm.exception), UNMATCHED_DELIMITER_ERROR_MSG.format(delimiter=delimiter, text=invalid_node.text))
-
-
-
 
 @dataclass
 class Input:
