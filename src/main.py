f1b275039e6301c6b3420ff0eb2a6090674ebea6
TobiasPartzsch <Tobias.Partzsch@gmx.de>
2025-06-07 14:44:31 +0200

    also generate page

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from file_operations import copy_dir
+from generator import generate_page
 
 
 dir_path_static = "./static"
@@ -8,6 +9,8 @@ dir_path_public = "./public"
 def main():
     print("Copying static files to public directory...")
     copy_dir(dir_path_static, dir_path_public)
+    generate_page("content/index.md", "template.html", "public/index.html")
+
 
 if __name__ == "__main__":
     main()
