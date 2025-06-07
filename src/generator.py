4f643073ef8af5deaf5fc4f5507ac990e4648e92
TobiasPartzsch <Tobias.Partzsch@gmx.de>
2025-06-07 15:24:01 +0200

    supports basepath now

--- src/generator.py
+++ src/generator.py
@@ -11,7 +11,12 @@ def extract_title(markdown: str) -> str:
     raise ValueError(f"No first-level heading found in {"\n".join(markdown.split('\n'))}")
 
 
-def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
+def generate_page(
+        from_path: str,
+        template_path: str,
+        dest_path: str,
+        basepath: str
+    ) -> None:
     print(f"Generating page from {from_path} to {dest_path} using {template_path}.")
     with open(from_path) as from_file:
         from_content = from_file.read()
@@ -24,6 +29,8 @@ def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
 
     template_content = template_content.replace("{{ Title }}", title)
     template_content = template_content.replace("{{ Content }}", content)
+    template_content = template_content.replace("href=\"/", "href=\"{basepath}")
+    template_content = template_content.replace("src=\"/", "src=\"{basepath}")
 
     # make sure dest_path directory exists
     dest_dir = os.path.dirname(dest_path)
@@ -34,14 +41,29 @@ def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
         dest_file.write(template_content)
 
 
-def generate_pages_recursive(dir_path_content: str, template_path: str, dest_dir_path: str) -> None:
+def generate_pages_recursive(
+        dir_path_content: str,
+        template_path: str,
+        dest_dir_path: str,
+        basepath: str,
+    ) -> None:
     print(f"Generating pages in {dir_path_content} to {dest_dir_path} using {template_path}.")
     for entry in os.listdir(dir_path_content):
         entry_path = os.path.join(dir_path_content, entry)
         f, ext = os.path.splitext(entry)
         if os.path.isdir(entry_path):
-            generate_pages_recursive(entry_path, template_path, os.path.join(dest_dir_path, entry))
+            generate_pages_recursive(
+                entry_path,
+                template_path,
+                os.path.join(dest_dir_path, entry),
+                basepath,
+            )
         elif ext.lower() == MD_EXT:
-            generate_page(entry_path, template_path, os.path.join(dest_dir_path, f"{f}.html"))
+            generate_page(
+                entry_path,
+                template_path,
+                os.path.join(dest_dir_path, f"{f}.html"),
+                basepath,
+            )
         else:
             print(f"ignore {entry}")
--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import sys
 from file_operations import copy_dir
 from generator import generate_pages_recursive
 
@@ -7,9 +8,11 @@ dir_path_public = "./public"
 
 
 def main():
+    basepath = sys.argv[0] or '/'
+
     print("Copying static files to public directory...")
     copy_dir(dir_path_static, dir_path_public)
-    generate_pages_recursive("content", "template.html", "public")
+    generate_pages_recursive("content", "template.html", "public", basepath)
 
 
 if __name__ == "__main__":
